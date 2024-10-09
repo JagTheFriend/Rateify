eab7bb92534c5b2a7212d97985ad37e574820335
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-09 21:20:54 +0530

    Added public routes
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,11 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+const isPublicRoute = createRouteMatcher(['/', '/(contact|about)', '/auth(.*)'])
+
+export default clerkMiddleware((auth, req) => {
+  // Restrict dashboard routes to signed in users
+  if (!isPublicRoute(req)) auth().protect()
+})
 
 export const config = {
   matcher: [
