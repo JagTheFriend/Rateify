2730bff4f050e27ff1b5932c8db3d294c724c0a0
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-09 16:04:39 +0530

    Added border
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,19 +4,10 @@ import { TypewriterEffectSmooth } from '~/components/ui/typewriter-effect'
 
 const words = [
   {
-    text: 'Build',
+    text: 'About',
   },
   {
-    text: 'awesome',
-  },
-  {
-    text: 'apps',
-  },
-  {
-    text: 'with',
-  },
-  {
-    text: 'Aceternity.',
+    text: 'Rateify.',
     className: 'text-blue-500 dark:text-blue-500',
   },
 ]
@@ -24,19 +15,11 @@ const words = [
 export default function AboutPage() {
   return (
     <section>
-      <div className="flex flex-col items-center justify-center h-[40rem]  ">
-        <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
-          The road to freedom starts from here
-        </p>
-        <TypewriterEffectSmooth words={words} />
-        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-          <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
-            Join now
-          </button>
-          <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
-            Signup
-          </button>
-        </div>
+      <div className="flex flex-col items-center h-[40rem] text-xl">
+        <TypewriterEffectSmooth
+          words={words}
+          className="border-b-2 border-violet-500"
+        />
       </div>
     </section>
   )
