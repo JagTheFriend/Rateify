66e9a8d34162de78821e90b9b9fd8621fb5d7e53
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-09 20:40:44 +0530

    Working on auth page
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+import Footer from '~/components/footer'
+import Navbar from '../_components/Navbar'
+
+export const metadata: Metadata = {
+  title: 'Sign-in/Register for Rateify',
+  description: 'Authentication page for Rateify',
+  icons: [{ rel: 'icon', url: '/favicon.ico' }],
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <>
+      <main className="flex flex-col">
+        <Navbar />
+        {children}
+      </main>
+      <Footer />
+    </>
+  )
+}
