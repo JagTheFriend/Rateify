07b3d58e30b58c0d0b588fcdcfa7406f99774391
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-09 18:26:09 +0530

    Working on cards
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import ContactDetailsCards from './_components/ContactDetailsCards'
 import TypewriterHeading from './_components/TypeWritter'
 
 export default function ContactUs() {
   return (
     <>
       <TypewriterHeading />
+      <ContactDetailsCards />
     </>
   )
 }
