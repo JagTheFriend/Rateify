381a99fc5d1edf33758046294903c51c5ad9931a
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-09 14:14:34 +0530

    Created a simple intro
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import IntroSection from './_components/Intro'
 import NavbarComponent from './_components/Navbar'
 
 export default function HomePage() {
   return (
     <main className="flex flex-col">
       <NavbarComponent />
+      <IntroSection />
     </main>
   )
 }
