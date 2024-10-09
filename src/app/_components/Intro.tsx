02b0e5c6b126cca1e5058ab079068119ea4ed33a
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-09 14:50:24 +0530

    Changed height
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/_components/Intro.tsx
+++ src/app/_components/Intro.tsx
@@ -4,7 +4,7 @@ import { BackgroundLines } from '~/components/ui/background-lines'
 
 export default function IntroSection() {
   return (
-    <section className="flex flex-col items-center justify-center -z-10">
+    <section className="flex flex-col items-center justify-center -z-10 h-screen">
       <BackgroundLines className="flex items-center justify-center w-full flex-col">
         <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-400 to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
           Rateify
