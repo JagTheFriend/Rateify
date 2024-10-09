5dbc6ddee3ecacd589e3c07cf6672a3356123b5b
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-09 14:44:00 +0530

    Fixing UI bugs
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/_components/FeaturesCards.tsx
+++ src/app/_components/FeaturesCards.tsx
@@ -7,25 +7,22 @@ const projects = [
     title: 'Easy Interaction',
     description:
       "It's simple and fun to rate others' photos, creating a fast and enjoyable way to engage with the community.",
-    link: '#',
   },
   {
     title: 'Discover Popular Trends',
     description:
       'By viewing top-rated photos, you can stay on top of trends and gain inspiration for their next post.',
-    link: '#',
   },
   {
     title: 'Personalized Insights',
     description:
       'Get tailored feedback on your photos based on user preferences and trends.',
-    link: '#',
   },
 ]
 
 export default function FeaturesCards() {
   return (
-    <section className="max-w-5xl mx-auto px-8">
+    <section id="features-section" className="max-w-5xl mx-auto px-8">
       <HoverEffect items={projects} />
     </section>
   )
