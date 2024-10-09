84e9660de655747ccf51fd06031308a1acc00e4d
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-09 21:09:52 +0530

    Created a basic navbar
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/_authorized/page.tsx
+++ src/app/_authorized/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 
 import Footer from '~/components/footer'
+import NavbarComponent from './_components/Navbar'
 
 export default function LandingPageAuthorized() {
   return (
     <main className="flex flex-col h-screen">
-      <div className="flex-grow">Hello world</div>
+      <div className="flex-grow">
+        <NavbarComponent />
+      </div>
       <Footer />
     </main>
   )
