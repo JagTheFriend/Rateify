9b8931af16f4c120b99582b90fddf76752192c5d
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-10 13:13:20 +0530

    Check if user is authenticated or not
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/server/actions.ts
+++ src/server/actions.ts
@@ -1,6 +1,14 @@
 'use server'
 
+import { auth } from '@clerk/nextjs/server'
+
 export async function newPost(formData: FormData) {
+  const currentUser = auth()
+
+  if (!currentUser) {
+    return { message: 'Unauthenticated', status: 401 }
+  }
+
   console.log(`Title ${formData.get('post-title')}`)
   return ''
 }
