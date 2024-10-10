d01f7d98c1c9adcd65cad26371247fe687441f92
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-10 11:52:22 +0530

    Display images in carousel format
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/new/_components/EditImage.tsx
+++ src/app/new/_components/EditImage.tsx
@@ -1,3 +1,29 @@
+'use client'
+
+import { useAtomValue } from 'jotai'
+import { Carousel } from 'react-responsive-carousel'
+import { UploadedFiles } from './data'
+
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+
 export default function EditImage() {
-  return <section></section>
+  const uploadedFiles = useAtomValue(UploadedFiles)
+
+  return (
+    <section className="mx-4">
+      <Carousel
+        showArrows={true}
+        // onChange={onChange}
+        // onClickItem={onClickItem}
+        // onClickThumb={onClickThumb}
+      >
+        <div>
+          <img src={URL.createObjectURL(uploadedFiles?.[0] ?? new Blob())} />
+        </div>
+        <div>
+          <img src={URL.createObjectURL(uploadedFiles?.[1] ?? new Blob())} />
+        </div>
+      </Carousel>
+    </section>
+  )
 }
