ceb6f4fdf1e3edeb52ea75945a346fc4114fbb57
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-10 12:04:51 +0530

    Display a small card under the input field
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/new/_components/EditImage.tsx
+++ src/app/new/_components/EditImage.tsx
@@ -4,26 +4,30 @@ import { useAtomValue } from 'jotai'
 import { Carousel } from 'react-responsive-carousel'
 import { UploadedFiles } from './data'
 
+import { useState } from 'react'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
 export default function EditImage() {
   const uploadedFiles = useAtomValue(UploadedFiles)
+  const [currentIndex, setCurrentIndex] = useState(0)
 
   return (
     <section className="mx-4">
-      <Carousel
-        showArrows={true}
-        // onChange={onChange}
-        // onClickItem={onClickItem}
-        // onClickThumb={onClickThumb}
-      >
-        <div>
-          <img src={URL.createObjectURL(uploadedFiles?.[0] ?? new Blob())} />
+      {uploadedFiles?.length && uploadedFiles.length >= 1 ? (
+        <Carousel showArrows={true} onChange={(e) => setCurrentIndex(e)}>
+          {Array.from(uploadedFiles).map((file, index) => (
+            <div key={index + Math.random()}>
+              <img src={URL.createObjectURL(file as File)} />
+            </div>
+          ))}
+        </Carousel>
+      ) : (
+        <div className="card bg-base-100 text-neutral-content">
+          <div className="card-body items-center text-center">
+            <p className="text-gray-500">Select an Image to Preview it</p>
+          </div>
         </div>
-        <div>
-          <img src={URL.createObjectURL(uploadedFiles?.[1] ?? new Blob())} />
-        </div>
-      </Carousel>
+      )}
     </section>
   )
 }
