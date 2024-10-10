392dd40cae766b0c167dfe0d6e5bd05974a2afab
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-10 20:39:57 +0530

    Use firebase to for storing images
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/server/actions.ts
+++ src/server/actions.ts
@@ -1,6 +1,8 @@
 'use server'
 
 import { auth } from '@clerk/nextjs/server'
+import { getStorage, ref, uploadBytes } from 'firebase/storage'
+import { v4 as uuidv4 } from 'uuid'
 
 export async function newPost(formData: FormData) {
   const currentUser = auth()
@@ -10,10 +12,21 @@ export async function newPost(formData: FormData) {
   }
 
   const uploadData = {
+    postId: uuidv4(),
     title: formData.get('post-title') as string,
     description: formData.get('post-description') as string,
     imageFiles: formData.getAll('image') as File[],
   }
-  console.log(uploadData)
-  return ''
+
+  const storage = getStorage()
+  const storageRef = ref(storage, uploadData.postId)
+
+  try {
+    for (const file of uploadData.imageFiles) {
+      await uploadBytes(storageRef, file)
+    }
+    return { message: 'Uploaded Images', status: 200 }
+  } catch (error) {
+    return { message: 'Error Occur', status: 503 }
+  }
 }
