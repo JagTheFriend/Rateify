6a48a16e0b4d07e89e60b537ae3089a54c3a6e9a
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-10 20:40:42 +0530

    Export the firebaseApp
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,17 +1,33 @@
-import { PrismaClient } from "@prisma/client";
-
-import { env } from "~/env";
+import { PrismaClient } from '@prisma/client'
+import { initializeApp } from 'firebase/app'
+import { env } from '~/env'
 
 const createPrismaClient = () =>
   new PrismaClient({
     log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+      env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  })
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
-};
+const globalVariable = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient> | undefined
+  firebaseApp: ReturnType<typeof initializeApp> | undefined
+}
 
-export const db = globalForPrisma.prisma ?? createPrismaClient();
+export const db = globalVariable.prisma ?? createPrismaClient()
 
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+if (env.NODE_ENV !== 'production') globalVariable.prisma = db
+
+const firebaseConfig = {
+  apiKey: env.API_KEY,
+  authDomain: env.AUTH_DOMAIN,
+  projectId: env.PROJECT_ID,
+  storageBucket: env.STORAGE_BUCKET,
+  messagingSenderId: env.MESSAGING_SENDER_ID,
+  appId: env.APP_ID,
+}
+
+// Initialize Firebase
+export const firebaseApp =
+  globalVariable.firebaseApp ?? initializeApp(firebaseConfig)
+
+if (env.NODE_ENV !== 'production') globalVariable.firebaseApp = firebaseApp
