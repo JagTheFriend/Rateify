065467adc9df309dbcc9a8209eaecd041fc3a9d0
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-11 21:44:00 +0530

    Created a util function to format the number
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -21,3 +21,15 @@ export function getUserDetail(user: User): CustomUserType {
     dateJoined: user.createdAt,
   }
 }
+
+export function formatNumber(number: number): string {
+  // Use the toLocaleString method to add suffixes to the number
+  return number.toLocaleString('en-US', {
+    // add suffixes for thousands, millions, and billions
+    // the maximum number of decimal places to use
+    maximumFractionDigits: 2,
+    // specify the abbreviations to use for the suffixes
+    notation: 'compact',
+    compactDisplay: 'short',
+  })
+}
