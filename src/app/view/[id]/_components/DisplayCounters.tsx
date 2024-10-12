27b825b17f89589adf0dca5761a7c709c613ca64
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-12 19:42:36 +0530

    Format number before displaying
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/view/[id]/_components/DisplayCounters.tsx
+++ src/app/view/[id]/_components/DisplayCounters.tsx
@@ -2,6 +2,7 @@
 
 import { useOptimistic, useTransition } from 'react'
 import { toast } from 'sonner'
+import { formatNumber } from '~/lib/utils'
 import { likeOrDislikePost } from '~/server/post-actions'
 
 type Props = {
@@ -62,7 +63,7 @@ function LikeButton({ likeCounter, postId }: Partial<Props>) {
       >
         <path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path>
       </svg>
-      {optimisticLikeCounter}
+      {formatNumber(optimisticLikeCounter)}
     </button>
   )
 }
