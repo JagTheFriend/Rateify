d84cb2af6c33d24576f9e80723b410307ff94da4
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-13 13:27:55 +0530

    Fixing bugs
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/server/post-actions.ts
+++ src/server/post-actions.ts
@@ -185,7 +185,7 @@ export async function getListOfPosts(cursorId?: string) {
 
     const finalData: CustomPostType[] = posts.map((post) => ({
       ...post,
-      imageUrls: new Array(post.numberOfImages).map(
+      imageUrls: [...Array(post.numberOfImages).keys()].map(
         (_, index) =>
           `https://firebasestorage.googleapis.com/v0/b/rateify-17fc8.appspot.com/o/posts%2F${post.id}%2F${index}?alt=media`,
       ),
@@ -199,7 +199,6 @@ export async function getListOfPosts(cursorId?: string) {
           username: 'Unknown',
         } as CustomUserType),
     }))
-
     return { status: 203, message: finalData }
   } catch (error) {
     return {
