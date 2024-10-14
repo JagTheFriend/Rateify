c311b60368baf932361086dc9fd6fb0c0452d324
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-14 09:42:36 +0530

    Display a loading animation while the image is loading
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -1,9 +1,20 @@
 'use client'
 
+import { useRef, useState } from 'react'
 import { Carousel } from 'react-responsive-carousel'
 import 'react-responsive-carousel/lib/styles/carousel.min.css' // requires a loader
 
 export default function ImageCarousel({ imageUrls }: { imageUrls: string[] }) {
+  const [isLoading, setIsLoading] = useState(true)
+  const counter = useRef(0)
+
+  const isImageLoaded = () => {
+    counter.current += 1
+    if (counter.current >= imageUrls.length) {
+      setIsLoading(false)
+    }
+  }
+
   return (
     <section>
       <Carousel
@@ -16,7 +27,16 @@ export default function ImageCarousel({ imageUrls }: { imageUrls: string[] }) {
       >
         {imageUrls.map((url) => (
           <div key={Math.random()}>
-            <img src={url} />
+            <img
+              src={url}
+              onLoad={() => isImageLoaded()}
+              className={`${isLoading ? 'hidden' : ''}`}
+            />
+            {isLoading && (
+              <div className="flex flex-col gap-4 mx-4">
+                <div className="skeleton h-72 w-full shrink-0 rounded-md"></div>
+              </div>
+            )}
           </div>
         ))}
       </Carousel>
