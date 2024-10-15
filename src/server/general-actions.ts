ae5e257ae00fd0059ef7b79142de365890ba2990
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-15 19:31:13 +0530

    Perform validating when fetching user data
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/server/general-actions.ts
+++ src/server/general-actions.ts
@@ -1,21 +1,27 @@
 'use server'
 
 import { clerkClient } from '@clerk/nextjs/server'
-import type { CustomUserType } from '~/lib/types'
-import { getUserDetail } from '~/lib/utils'
+import { z } from 'zod'
+import { type CustomUserType, SERVER_ERROR_MESSAGE } from '~/lib/types'
+import { checkAuthentication, getUserDetail } from '~/lib/utils'
+
+const GetUserByIdSchema = z.object({
+  userId: z.string().min(1),
+})
 
 export async function getUserById(userId: string): Promise<{
-  status: 404 | 200
-  message: CustomUserType
+  status: 404 | 200 | number
+  message: CustomUserType | string
 }> {
   try {
+    GetUserByIdSchema.parse({
+      userId,
+    })
+    checkAuthentication()
     const user = await clerkClient().users.getUser(userId)
     const details = getUserDetail(user)
     return { status: 200, message: details }
   } catch (error) {
-    return {
-      status: 404,
-      message: {} as CustomUserType,
-    }
+    return SERVER_ERROR_MESSAGE
   }
 }
