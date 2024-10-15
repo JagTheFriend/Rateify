aa97f88c2a66e6da3ba9db335e40b2afcc6072d4
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-15 19:36:35 +0530

    Fixed types
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,5 +1,10 @@
+import { notFound } from 'next/navigation'
 import DisplayPosts from '~/app/_authorized/_components/DisplayPosts'
-import type { CustomPostType } from '~/lib/types'
+import type {
+  CustomPostType,
+  CustomUserType,
+  ReturnTypeOfPost,
+} from '~/lib/types'
 import { getUserById } from '~/server/general-actions'
 import { getListOfPosts } from '~/server/post-actions'
 
@@ -7,20 +12,31 @@ export default async function UserProfile({
   params,
 }: { params: { id: string } }) {
   const { id: userId } = params
-  const userData = await getUserById(userId)
-  const posts = await getListOfPosts(undefined, '', userId)
+  const { status: userStatus, message: userMessage } = await getUserById(userId)
+  const { status: postStatus, message: postMessage } = await getListOfPosts(
+    undefined,
+    '',
+    userId,
+  )
+
+  if (userStatus !== 200 || postStatus !== 200) {
+    return notFound()
+  }
+
+  const userData = userMessage as CustomUserType
+  const postData = postMessage as ReturnTypeOfPost[]
 
   return (
     <>
       <section className="py-2 mx-2 border-base-300 border-b flex flex-row items-center justify-center gap-2">
         <div className="avatar">
           <div className="w-10 rounded-full">
-            <img src={userData.message.image} />
+            <img src={userData.image} />
           </div>
         </div>
-        <p className="text-xl font-semibold">{userData.message.username}</p>
+        <p className="text-xl font-semibold">{userData.username}</p>
       </section>
-      <DisplayPosts initialPostData={posts.message as CustomPostType[]} />
+      <DisplayPosts initialPostData={postData as CustomPostType[]} />
     </>
   )
 }
